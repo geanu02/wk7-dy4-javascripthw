bfe40e0e5ef18b16070c44d0575fcf5a773a9f22
Gian Carlo Torres <geanu@outlook.com>
2023-05-18 14:28:20 -0400

    .gitignore included secret.json

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("Hello Momsh")
+
+const { clientId, clientSecret } = require('../secret.json')
+
+console.log(clientId)
+
+const apiAdd = 'https://accounts.spotify.com/api/token'
+
+function getToken() {}
+
+function getSong() {}
+
+function handleClick() {}
+
+function getSongData() {}
